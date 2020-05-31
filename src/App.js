ae09c933b9e91eaf81a699f0cf0c924e634a23d0
Mariana Rascon <nienorloth@gmail.com>
2020-05-30 19:08:20 -0500

    images displayed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Formulario from './components/Formulario';
+import ListadoImagenes from './components/ListadoImagenes'; 
 
 function App() {
+
+  //state de la App
+  const [ busqueda, guardarBusqueda ] = useState('');
+  const [ imagenes, guardarImagenes ] = useState([]);
+  
+  useEffect(() => {
+   const consultarAPI = async () => {
+      if(busqueda === '') return;
+
+      const imagenesPorPagina = 30;
+      const key = "16770867-269e834c87375857bdbe6429b";
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarImagenes(resultado.hits);
+   }
+   consultarAPI();
+  }, [busqueda])
+
   return (
-    <div>
-      <p>Pixabay</p>
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de imágenes</p>
+        <Formulario 
+          guardarBusqueda={guardarBusqueda}
+        />
+      </div>
+      <div className="row justify-content-center">
+        <ListadoImagenes 
+          imagenes={imagenes}
+        />
+      </div>
     </div>
   );
 }
